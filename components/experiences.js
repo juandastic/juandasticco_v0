1827e537819251fd02d9c19ff592d849d524488d
Juan David Gomez <juandastic@gmail.com>
2019-04-11 23:25:35 +0000

    Timeline mock

--- components/experiences.js
+++ components/experiences.js
@@ -6,6 +6,20 @@ const Experiences = () => {
   return (
     <div className="experiences">
       <h2>Experiences</h2>
+      <div class="timeline">
+        <div class="container left">
+          <div class="content">
+            <h2>2017</h2>
+            <p>Lorem ipsum..</p>
+          </div>
+        </div>
+        <div class="container right">
+          <div class="content">
+            <h2>2016</h2>
+            <p>Lorem ipsum..</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
